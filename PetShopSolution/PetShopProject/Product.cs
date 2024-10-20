e748939b50b636bf57d78a420d84a68e31625b8a
Megan Miller <wovenword707@gmail.com>
2024-10-20 15:50:16 -0400

    first upload of petshop. Its a mess

---
+++ PetShopSolution/PetShopProject/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShopProducts
+{
+    class Product
+    {
+        public string name;
+        public decimal price;
+        public int quantity;
+        public string description;
+
+    }
+    class CatFood : Product 
+    {
+        public double weightPounds;
+        public bool kittenFood;
+    
+    }
+    class DogLeash : Product
+    {
+        public int lengthInches;
+        public string material;
+
+    }
+
+}
+
---
+++ PetShopSolution/PetShopProject/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.ComponentModel.Design;
+using PetShopProducts;
+
+namespace PetShopProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ASK USER TO CREATE PRODUCT OR EXIT
+            Console.WriteLine("Press '1' to add a product.");
+            Console.WriteLine("Type 'exit' to quit.");
+
+            //GET USER INPUT
+            string userInput = Console.ReadLine();
+
+            //SAVE USER PRODUCT INPUT
+            string productChoice;
+
+            //METHOD TO CREATE PRODUCT
+            void CreateProduct(string productChoice)
+            { 
+            
+            }
+            
+            //RUN THIS WHILE INPUT IS NOT "EXIT"
+            while (userInput.ToLower() != "exit")
+            {
+                if (userInput == "1")
+                {
+                    Console.WriteLine("What Product? Choose from: cat food, dog leash...");
+                    productChoice = Console.ReadLine();
+
+                    if (productChoice.ToLower() == "cat food")
+                    {
+                        Console.WriteLine("New cat food: ");
+                        CatFood catFood = new CatFood();
+                        //name price quantity description kitten weightPounds
+                        Console.WriteLine("Enter name: ");
+                        catFood.name = Console.ReadLine();
+                        Console.WriteLine("Enter price: ");
+                        catFood.price = decimal.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter quantity: ");
+                        catFood.quantity = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter description: ");
+                        catFood.description = Console.ReadLine();
+                        Console.WriteLine("Kitten food (true or false): ");
+                        catFood.kittenFood = bool.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter weight (Pounds): ");
+                        catFood.weightPounds = double.Parse(Console.ReadLine());
+
+                        //DISPLAY PROPERTIES OF CREATED PRODUCT
+                        // "\n" CREATEES NEW LINE
+                        Console.WriteLine("Name: " + catFood.name + "\nPrice: " + catFood.price + "\nQuantity: " + catFood.quantity + "\nDescription: " + catFood.description + "\nKitten food: " + catFood.kittenFood + "\nWeight (Pounds): " + catFood.weightPounds);
+                    }
+                    else if (productChoice.ToLower() == "dog leash")
+                    {
+                        Console.WriteLine("New dog leash.");
+                        DogLeash dogLeash = new DogLeash();
+                    }
+                   
+
+
+
+                }
+
+
+
+                Console.WriteLine("Press '1' to add a product.");
+                Console.WriteLine("Type 'exit' to quit.");
+                userInput = Console.ReadLine();
+            }
+           
+        }
+    }
+    
+    
+}
+
+
