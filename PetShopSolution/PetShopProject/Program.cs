53c920e698a4e6ed1044ab3cbb4cca0a7282ff1d
Megan Miller <wovenword707@gmail.com>
2024-10-20 17:25:35 -0400

    fixing things

--- PetShopSolution/PetShopProject/Program.cs
+++ PetShopSolution/PetShopProject/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.ComponentModel.Design;
-using PetShopProducts;
+using System.Text.Json;
 
 namespace PetShopProject
 {
@@ -51,13 +51,34 @@ namespace PetShopProject
                         catFood.weightPounds = double.Parse(Console.ReadLine());
 
                         //DISPLAY PROPERTIES OF CREATED PRODUCT
-                        // "\n" CREATEES NEW LINE
-                        Console.WriteLine("Name: " + catFood.name + "\nPrice: " + catFood.price + "\nQuantity: " + catFood.quantity + "\nDescription: " + catFood.description + "\nKitten food: " + catFood.kittenFood + "\nWeight (Pounds): " + catFood.weightPounds);
+                        // "\n" CREATES NEW LINE
+                        //Console.WriteLine("Name: " + catFood.name + "\nPrice: " + catFood.price + "\nQuantity: " + catFood.quantity + 
+                        //    "\nDescription: " + catFood.description + "\nKitten food: " + catFood.kittenFood + "\nWeight (Pounds): " + catFood.weightPounds);
+                        Console.WriteLine(JsonSerializer.Serialize(catFood));
+
                     }
                     else if (productChoice.ToLower() == "dog leash")
                     {
                         Console.WriteLine("New dog leash.");
                         DogLeash dogLeash = new DogLeash();
+                        //name price quantity description lengthInches material
+                        Console.WriteLine("Enter name: ");
+                        dogLeash.name = Console.ReadLine();
+                        Console.WriteLine("Enter price: ");
+                        dogLeash.price = decimal.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter quantity: ");
+                        dogLeash.quantity = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter description: ");
+                        dogLeash.description = Console.ReadLine();
+                        Console.WriteLine("Enter length (inches): ");
+                        dogLeash.lengthInches = int.Parse(Console.ReadLine());
+                        Console.WriteLine(" : ");
+                        dogLeash.material = Console.ReadLine();
+
+                        //DISPLAY PROPERTIES OF CREATED PRODUCT
+                        // "\n" CREATES NEW LINE
+                        Console.WriteLine("Name: " + dogLeash.name + "\nPrice: " + dogLeash.price + "\nQuantity: " + dogLeash.quantity + 
+                            "\nDescription: " + dogLeash.description + "\nEnter length (inches): " + dogLeash.lengthInches + "\nEnter material: " + dogLeash.material);
                     }
                    
 
